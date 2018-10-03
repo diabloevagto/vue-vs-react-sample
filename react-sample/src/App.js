cc50e65577d2dd32ba12f3b639a09547d144a58c
diabloevagto <diabloevagto@gmail.com>
2018-10-03 19:12:19 +0800

    feat: 清除用不到的部分

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            style={{
+              animation: 'App-logo-spin infinite 20s linear',
+              height: '40vmin'
+            }}
+          />
         </header>
       </div>
     );
